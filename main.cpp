d41075765c023df2a6093ea0a4c78fbf2261e330
Jason Schafer <jschafer@lakotasoftware.com>
2021-09-24 15:37:52 -0400

    Fixed Linkage & Added Usage of all Functions

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
  */
 
 #include <iostream>
-#include <MundyMath.h>
+#include <string>
+#include "MundyMath.h"
 
 int main() {
-	std::cout << "2+2=" << MundyMath::add(2, 2);
+	std::cout << "Welcome to the Fluffy Succotash App! Let's do some Math! :) \n";
+	std::cout << "2+2=" + to_string(MundyMath::add(2, 2).getResult()) + "\n";
+	std::cout << "2-2=" + to_string(MundyMath::subtract(2, 2).getResult()) + "\n";
+	std::cout << "3*3=" + to_string(MundyMath::multiply(2, 2).getResult()) + "\n";
+	std::cout << "3/3=" + to_string(MundyMath::divide(2, 2).getResult()) + "\n";
+	std::cout << "sqrt(9)=" + to_string(MundyMath::squareRoot(9).getResult()) + "\n";
+	std::cout << "10^2=" + to_string(MundyMath::power(10, 2).getResult()) + "\n";
+	std::cout << "That's enough math for today! Goodbye! :) \n";
 	return 0;
 }
